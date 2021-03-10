cdb31c374363caa73c7c5a787e824bdfa9f56d72
koudjo <kametepe@gmail.com>
2021-03-10 17:24:18 +0100

    save implementations

--- ThemePark/Startup.cs
+++ ThemePark/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ThemePark.Infrastructure;
 using ThemePark.Repositories.Implementations;
 using ThemePark.Repositories.Interfaces;
 using ThemePark.Services.Implementations;
@@ -28,6 +30,10 @@ namespace ThemePark
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connection = Configuration.GetConnectionString("XLABBackOfficeConnection");
+            services.AddDbContext<ThemeParkContext>
+                (options => options.UseSqlServer(connection));
+
             services.AddScoped<IRideService, RideService>();
             services.AddScoped<IRideRepository, RideRepository>();
 
