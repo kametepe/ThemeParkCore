a0dd1be778b855514e44901e78d36483d0758b5a
koudjo <kametepe@gmail.com>
2021-03-11 10:04:21 +0100

    Add Swagger

--- ThemePark/Startup.cs
+++ ThemePark/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,6 +39,23 @@ namespace ThemePark
             services.AddScoped<IRideRepository, RideRepository>();
 
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ThemePark API",
+                    Description = "A simple example ASP.NET Core Web API",                    
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Koudjo AMETEPE",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/kametepe"),
+                    } 
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +73,13 @@ namespace ThemePark
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ThemePark API V1");
+                c.RoutePrefix = string.Empty;
             });
         }
     }
