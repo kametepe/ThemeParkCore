2e82879d7938cad19cff477612cbc02af97ba814
koudjo <kametepe@gmail.com>
2021-03-11 09:56:06 +0100

    Add Swagger to Core 2.2

--- ThemePark/Startup.cs
+++ ThemePark/Startup.cs
@@ -4,16 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
 using ThemePark.Infrastructure;
 using ThemePark.Repositories.Implementations;
 using ThemePark.Repositories.Interfaces;
 using ThemePark.Services.Implementations;
 using ThemePark.Services.Interfaces;
+using Microsoft.OpenApi.Models;
 
 namespace ThemePark
 {
@@ -37,6 +35,24 @@ namespace ThemePark
             services.AddScoped<IRideRepository, RideRepository>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ThemePark API",
+                    Description = "A simple example ASP.NET Core Web API",                    
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Koudjo AMETEPE",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/kametepe"),
+                    }
+                    
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +67,13 @@ namespace ThemePark
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ThemePark API V1");
+                c.RoutePrefix = string.Empty;
+            });
         }
     }
 }
